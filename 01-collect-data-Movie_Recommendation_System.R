e984c1c657d1d9cd0fdb7b16e032fa9d3ae50a1d
DianneSvdS <dianne.van.der.spek@topigsnorsvin.com>
2023-10-10 17:41:56 +0200

    basic files

---
+++ 01-collect-data-Movie_Recommendation_System.R
@@ -0,0 +1,16 @@
+# Build a recommendation engine that recommends movies to users.
+# Item Based Collaborative Filter recommendation system
+#install packages if needed:
+install.packages(c("here", "recommenderlab", "ggplot2", "data.table", "reshape2"))
+
+library(recommenderlab)
+library(ggplot2)                       
+library(data.table)
+library(reshape2)
+library(here)
+
+# Retrieve and display data
+movie_data <- read.csv(here("IMDB-Dataset", "movies.csv"),stringsAsFactors=FALSE)
+rating_data <- read.csv(here("IMDB-Dataset", "ratings.csv"))
+str(movie_data)
+
